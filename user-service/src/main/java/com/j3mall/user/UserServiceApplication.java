566fdec301df0997b27c2ba267500c5fd28f8664
Teddy Jiang <teddy.jiang@qq.com>
2023-03-25 21:58:16 +0800

    Revert "maven clean package As war"
    This reverts commit a4aba867d90f55a39dd21fa94b83692d320579ff.

--- user-service/src/main/java/com/j3mall/user/UserServiceApplication.java
+++ user-service/src/main/java/com/j3mall/user/UserServiceApplication.java
@@ -4,8 +4,6 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,7 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.j3mall.user", "com.j3mall.framework"})
 @MapperScan("com.j3mall.user.mybatis.mapper")
-public class UserServiceApplication extends SpringBootServletInitializer {
+public class UserServiceApplication {
+
 	@Value("${spring.application.name}:${server.port}")
 	private String serverHost;
 
@@ -24,13 +23,7 @@ public class UserServiceApplication extends SpringBootServletInitializer {
 	}
 
 	public static void main(String[] args) {
-		Long time=System.currentTimeMillis();
 		SpringApplication.run(UserServiceApplication.class, args);
-		System.out.printf("===应用启动耗时：%s毫秒===%n", System.currentTimeMillis() - time);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(this.getClass());
-	}
 }
